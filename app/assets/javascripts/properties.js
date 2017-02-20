52fedb44da880732fbc243382fcc45e34ddaaf4e
Tomy8s <tomy8s@hotmail.com>
2017-02-20 20:43:09 +0000

    Returns place_ids when given a location

--- app/assets/javascripts/properties.js
+++ app/assets/javascripts/properties.js
@@ -4,6 +4,13 @@ $(document).on('turbolinks:load', function () {
     var searchLocation = $('#location_input').val();
     var url = '/properties/search/?location=' + searchLocation
     console.log(url);
-    $.ajax({url: url}).done(function(results){console.log(results)})
+    $.ajax({url: url}).done(function(results){
+      console.log(results);
+      // var locations = results.result.locations
+      // // console.log(locations);
+      // for(var i = 0; i < locations.elements.length; i++) {
+      //   console.log(locations.elements[i].place_id);
+      // };
+    });
   })
 });
--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class PropertiesController < ApplicationController
   def index
@@ -6,8 +7,14 @@ class PropertiesController < ApplicationController
 
   def search
     url = 'http://index1.homeflow.co.uk/places?api_key=' + ENV['API_key'] + '&search[name]=' + params[:location]
-    response = HTTParty.get(url)
-    render :json => response.parsed_response
+    response = HTTParty.get(url).parsed_response
+    #parsed = JSON.parse(response.parsed_response)
+    send_back = []
+    response["result"]["locations"]["elements"].each { |l| send_back << l["place_id"]}
+    puts "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+    p response["result"]["locations"]["elements"].size
+    puts "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+    render :json => send_back
   end
 
   def show
