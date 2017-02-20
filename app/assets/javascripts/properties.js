6ff8824deb24f0565bbc9c6844db61d3a29fc154
Tomy8s <tomy8s@hotmail.com>
2017-02-20 19:02:47 +0000

    Jquery constructs query URL

--- app/assets/javascripts/properties.js
+++ app/assets/javascripts/properties.js
@@ -1,6 +1,8 @@
 $(document).on('turbolinks:load', function () {
-    $("#location_input").keyup(function() {
-    var searchLocation = $("#location_input").val();
-    console.log(searchLocation);
+  var aPIKey = $('#API_key').val();
+    $('#location_input').keyup(function() {
+    var searchLocation = $('#location_input').val();
+    var url = 'http://index1.homeflow.co.uk/places?api_key=' + aPIKey + '&search[name]=' + searchLocation
+    console.log(url);
   })
 });
