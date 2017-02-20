ab2631783ee42ea3eebabe0eac12cfed0872d36d
Tomy8s <tomy8s@hotmail.com>
2017-02-20 20:52:41 +0000

    returns an array of place objects

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -9,11 +9,18 @@ class PropertiesController < ApplicationController
     url = 'http://index1.homeflow.co.uk/places?api_key=' + ENV['API_key'] + '&search[name]=' + params[:location]
     response = HTTParty.get(url).parsed_response
     #parsed = JSON.parse(response.parsed_response)
+    place_ids = []
+    response["result"]["locations"]["elements"].each { |l| place_ids << l["place_id"]}
     send_back = []
-    response["result"]["locations"]["elements"].each { |l| send_back << l["place_id"]}
+    place_ids.each do |p|
+      place_url = 'http://index1.homeflow.co.uk/properties/' + p + '?api_key=' + ENV['API_key']
+      place_response = HTTParty.get(url).parsed_response
+      send_back << place_response
+    end
     puts "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
     p response["result"]["locations"]["elements"].size
     puts "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+    # send_back = place_ids
     render :json => send_back
   end
 
